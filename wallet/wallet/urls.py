7faeae286cd5c08fc80100fffc272efcf56c13b2
isaiaspereira307 <isaias.pereira@protonmail.com>
2024-08-13 11:51:51 +0000

    fix: fix importation core in urls

--- wallet/wallet/urls.py
+++ wallet/wallet/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('wallet.core.urls')),
+    path('api/v1/', include('core.urls')),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
