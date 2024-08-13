731a37c7c0782d59209920bd6ad9f686b911198a
isaiaspereira307 <isaias.pereira@protonmail.com>
2024-08-13 12:42:26 +0000

    feat: add get_bitcoin_price in serializer

--- wallet/core/serializers.py
+++ wallet/core/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Transaction, BankAccount, Loan, Investment, FixedExpense, Bitcoin
+from .crypto_utils import get_bitcoin_price
 
 
 class TransactionSerializer(serializers.ModelSerializer):
@@ -33,8 +34,13 @@ class FixedExpenseSerializer(serializers.ModelSerializer):
 
 
 class BitcoinSerializer(serializers.ModelSerializer):
+    current_btc_price = serializers.SerializerMethodField()
+
     class Meta:
         model = Bitcoin
-        fields = "__all__"
+        fields = ['bank_account', 'purchase_price', 'quantity', 'purchase_date', 'current_btc_price']
+
+    def get_current_btc_price(self, obj):
+        return get_bitcoin_price('USD')
 
 
